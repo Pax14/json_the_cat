01fa2115da65ca8b252dbb6c6fdcd81055dde3bb
Paxon Cheung <paxon.cheung@gmail.com>
2021-01-17 10:01:43 +0000

    refactored code and added test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,20 @@
 const request = require('request');
-const search = "https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2];
 
-request(search, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log('error:', error);
-  } else if (data.length === 0) {
-    console.log('error: breed not found');
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+const search = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+
+  request(search, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback('Breed not found', null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
